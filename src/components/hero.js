6ef9aad2529424edf45c836d18bb6065ad5d1712
toufanialfarisi <toufani.deeplearning@gmail.com>
2023-04-09 16:58:49 +0700

    bug fix

--- src/components/hero.js
+++ src/components/hero.js
@@ -5,10 +5,10 @@ function Hero() {
   return (
     <div className="text-white">
       <div className="max-w-[800px] mt-[-96px] w-full h-screen mx-auto flex flex-col text-center justify-center">
-        <p className="text-[#00df9a] font-bold p-2">
+        <p className="text-[#00df9a] md:text-4xl sm:text-4xl text-xl font-bold mb-4">
           GROWING WITH DATA ANALYTICS
         </p>
-        <h1 className="md:text-7xl sm:text-6xl text-4xl font-bold md:py-6">
+        <h1 className="md:text-7xl sm:text-6xl text-4xl font-bold mb-2">
           Grow with data.
         </h1>
         <div className="flex justify-center items-center">
@@ -16,14 +16,14 @@ function Hero() {
             Fast, flexible financing for
           </p>
           <Typed
-            className="md:text-5xl sm:text-4xl text-xl font-bold pl-2 md:pl-4"
+            className="md:text-5xl sm:text-4xl text-xl font-bold pl-2 md:pl-4 "
             strings={["BTB", "BTC", "SASS"]}
             typeSpeed={120}
             backSpeed={140}
             loop
           />
         </div>
-        <p className="md:text-2xl text-xl font-bold text-gray-500">
+        <p className="md:text-2xl text-xl font-bold text-gray-500 mt-4">
           Monitor your data analytics to increase revenue for BTB, BTC, & SAAS
           platform
         </p>
--- src/components/navbar.js
+++ src/components/navbar.js
@@ -2,7 +2,7 @@ import React from "react";
 import { AiOutlineClose, AiOutlineMenu } from "react-icons/ai";
 
 function Navbar() {
-  const [nav, setNav] = React.useState(false);
+  const [nav, setNav] = React.useState(true);
 
   const handleNav = () => {
     setNav(!nav);
@@ -10,32 +10,56 @@ function Navbar() {
 
   return (
     <div className="flex justify-between items-center h-24 max-w-[1240px] mx-auto px-4 text-white">
-      <h1 className="w-full text-3xl font-bold text-[#00df9a]">REACT.</h1>
+      <h1 className="w-full  text-3xl font-bold text-[#00df9a]">REACT.</h1>
       <ul className="hidden md:flex">
-        <li className="p-4">Home</li>
-        <li className="p-4">Company</li>
-        <li className="p-4">Resources</li>
-        <li className="p-4">About</li>
-        <li className="p-4">Contact</li>
+        <li className="p-4 ">
+          <a href="#">Home</a>
+        </li>
+        <li className="p-4 ">
+          <a href="#">Company</a>
+        </li>
+        <li className="p-4">
+          <a href="#">Resources</a>
+        </li>
+        <li className="p-4">
+          <a href="#">About</a>
+        </li>
+        <li className="p-4">
+          <a href="#">Contact</a>
+        </li>
       </ul>
       <div onClick={handleNav} className="block md:hidden">
         {!nav ? <AiOutlineClose size={20} /> : <AiOutlineMenu size={20} />}
       </div>
-      <div
-        className={
-          !nav
-            ? "fixed left-0 top-0 w-[60%] h-full border-r border-r-gray-900 bg-[#000300] ease-in-out duration-500"
-            : "fixed left-[-100%]"
-        }
-      >
-        <h1 className="w-full text-3xl font-bold text-[#00df9a] m-4">REACT.</h1>
-        <ul className="uppercase p-4">
-          <li className="p-4 border-b border-gray-600">Home</li>
-          <li className="p-4 border-b border-gray-600">Company</li>
-          <li className="p-4 border-b border-gray-600">Resources</li>
-          <li className="p-4 border-b border-gray-600">About</li>
-          <li className="p-4 border-b border-gray-600">Contact</li>
-        </ul>
+      <div className="md:hidden">
+        <div
+          className={
+            !nav
+              ? "fixed left-0 top-0 w-[60%] h-full border-r border-r-gray-900 bg-[#000300] ease-in-out duration-500"
+              : "fixed left-[-100%]"
+          }
+        >
+          <h1 className="w-full text-3xl font-bold text-[#00df9a] m-4">
+            REACT.
+          </h1>
+          <ul className="uppercase p-4">
+            <li className="p-4 border-b border-gray-600">
+              <a href="#">Home</a>
+            </li>
+            <li className="p-4 border-b border-gray-600">
+              <a href="#">Company</a>
+            </li>
+            <li className="p-4 border-b border-gray-600">
+              <a href="#">Resources</a>
+            </li>
+            <li className="p-4 border-b border-gray-600">
+              <a href="#">About</a>
+            </li>
+            <li className="p-4 border-b border-gray-600">
+              <a href="#">Contact</a>
+            </li>
+          </ul>
+        </div>
       </div>
     </div>
   );
